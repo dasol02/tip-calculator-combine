a9eb28d7542f41a16768edf40aaf665bc29d3820
dasol02 <leedasol2169@gmail.com>
2023-09-04 16:17:25 +0900

    # refactoring

--- tip_calculator_swiftUI/tip_calculator_swiftUI/Extenstion/UIView.swift
+++ tip_calculator_swiftUI/tip_calculator_swiftUI/Extenstion/UIView.swift
@@ -5,33 +5,7 @@
 //  Created by dasol lee on 2023/08/25.
 //
 
-import UIKit
 import SwiftUI
-
-extension UIView {
-    func addShadow(offset: CGSize, color: UIColor, radius: CGFloat, opacity: Float) {
-        layer.cornerRadius = radius
-        layer.masksToBounds = false
-        layer.shadowOffset = offset
-        layer.shadowColor = color.cgColor
-        layer.shadowRadius = radius
-        layer.shadowOpacity = opacity
-        let backgroundCGColor = backgroundColor?.cgColor
-        backgroundColor = nil
-        layer.backgroundColor = backgroundCGColor
-    }
-    
-    func addCornerRadius(radius: CGFloat) {
-        layer.masksToBounds = false
-        layer.cornerRadius = radius
-    }
-    
-    func addRoundedCorners(corners: CACornerMask, radius: CGFloat) {
-        layer.cornerRadius = radius
-        layer.maskedCorners = [corners]
-    }
-}
-
 
 extension View {
     func hideKeyboard() {
